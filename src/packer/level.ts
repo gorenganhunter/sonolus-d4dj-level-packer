ee930041f6e844daf75dccc89e10aa88988c8dae
gorenganhunter <gorengandev@gmail.com>
2025-01-20 02:32:14 +0700

    Support D4C Chart

--- src/packer/level.ts
+++ src/packer/level.ts
@@ -1,11 +1,11 @@
-import { b34djToLevelData, type B34DJChart } from 'sonolus-d4dj-engine'
+import { b34djToLevelData, d4cToLevelData, type B34DJChart, type D4CChart } from 'sonolus-d4dj-engine'
 import type { PackLevelData } from '.'
 
 export const packLevelData: PackLevelData = async ({ chart, offset }) => {
     if (!chart) throw new Error('No chart file selected')
 
-    const b34djChart = JSON.parse(await chart.text()) as B34DJChart
-    const levelData = b34djToLevelData(b34djChart, offset / 1000)
+    const chartJson = JSON.parse(await chart.text()) as B34DJChart | D4CChart
+    const levelData = !chartJson.MusicName ? d4cToLevelData(chartJson, offset / 1000) : b34djToLevelData(chartJson, offset / 1000)
 
     return { type: 'json', data: levelData }
 }
